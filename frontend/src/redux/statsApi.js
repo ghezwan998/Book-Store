9a8918f68585f42c6ccb5f89ade24ac42cd7679b
Ghezwan <ghezwan998@gmail.com>
2025-08-27 01:15:23 +0300

    Update statsApi.js

--- frontend/src/redux/statsApi.js
+++ frontend/src/redux/statsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const statsApi = createApi({
   reducerPath: 'statsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api/stats', credentials: 'include' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://book-store-backend-9u3q.onrender.com/api/stats', credentials: 'include' }),
   endpoints: (build) => ({
     getStats: build.query({
       query: () => '/',
