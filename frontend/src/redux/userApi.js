71b4d1eb8df041a33eee97ed63e8f1d881f520fd
Ghezwan <ghezwan998@gmail.com>
2025-08-27 01:15:43 +0300

    Update userApi.js

--- frontend/src/redux/userApi.js
+++ frontend/src/redux/userApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/auth",
+    baseUrl: "https://book-store-backend-9u3q.onrender.com/api/auth",
     credentials: "include",
   }),
   tagTypes: ["User"],
